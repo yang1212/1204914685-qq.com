a4a7acd5f0d50e9301c309823aa2e70391c0368f
yang_fu <yangfu@i72.com>
2021-10-30 09:16:47 +0800

    feat: vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -23,5 +23,15 @@ export default defineConfig({
         rewrite: (path) => path.replace(new RegExp('^/api'), '') // 前端通过/api为Tag匹配接口转发，而实际后端提供接口无/api的路径，故需要通过rewrite重写去除，从而连通接口
       }
     }
+  },
+  build:{
+    assetsDir: 'static/img/',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+      }
+    }
   }
 })
