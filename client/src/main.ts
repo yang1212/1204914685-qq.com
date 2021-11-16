07cefcc911df6fa3f5fd7b41593cfaa4085bb454
yang_fu <yangfu@i72.com>
2021-11-16 11:36:14 +0800

    feat: Element-plus由按需引入改为全局引入
    unplugin-vue-components插件的使用使得ElMessage等样式失效

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { Delete, CirclePlus, ArrowDown } from '@element-plus/icons'
 import App from './App.vue'
 import router from './router'
@@ -8,7 +10,7 @@ const app = createApp(App)
 app.component('Delete', Delete)
 app.component('CirclePlus', CirclePlus)
 app.component('ArrowDown', ArrowDown)
-
+app.use(ElementPlus)
 app.use(store)
 app.use(router)
 app.mount('#app')
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// import Components from 'unplugin-vue-components/vite'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    })
+    vue()
+    // Components({
+    //   resolvers: [ElementPlusResolver()],
+    // })
   ],
   resolve: {
     alias: {
